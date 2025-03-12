ad3dabdbd782ac861ef79a18cb8f79778471dbef
stephanedecerf <decerfstephane1972@gmail.com>
2025-03-12 14:07:08 +0100

    Home

--- src/Controller/controller-home.php
+++ src/Controller/controller-home.php
@@ -6,6 +6,13 @@ session_start();
 // on charge le fichier de config
 require_once '../../config.php';
 
+// on controle si la personne est bien loggée
+if (!isset($_SESSION['user_id'])) {
+    // on renvoie vers la page d'accueil si non
+    header('Location: ../../public/');
+    exit;
+}
+
 // connexion à la base de données via PDO (PHP Data Objects) = création instance
 $pdo = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8', DB_USER, DB_PASS);
 
@@ -13,14 +20,14 @@ $pdo = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8',
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 // requete SQL me permettant de rechercher tous les posts
-$sql = "SELECT * FROM `76_posts` NATURAL JOIN `76_pictures` WHERE `user_id` = " . $_SESSION['user_id'] . " ORDER BY `post_timestamp` DESC;";
+$sql = "SELECT * FROM `76_posts` NATURAL JOIN `76_users` NATURAL JOIN `76_pictures` WHERE `user_id` IN (
+(SELECT GROUP_CONCAT(fav_id) FROM 76_favorites WHERE `user_id` = " . $_SESSION['user_id'] . " GROUP BY `user_id`)," . $_SESSION['user_id'] . ") ORDER BY `post_timestamp` DESC;";
 
 // on prepare la requete pour se prémunir des injections SQL
 $stmt = $pdo->query($sql);
 
 // on stock le resultat de la requête dans un tableau associatif
 $allPosts = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
 
 $pdo = '';
 
--- src/View/view-home.php
+++ src/View/view-home.php
@@ -9,40 +9,31 @@ include_once '../../Templates/head.php' ?>
 
     <div class="border border-success text-center">
 
-        <div class="row row-cols-3 d-flex justify-content-center">
+        <div class="row row-cols-3 d-flex flex-column align-items-center justify-content-center">
 
-            <div class="card col-lg-3 col-11">
-                <h5 class="card-title"><img src="../../assets/img/users/11/voiture.png" class="rounded-circle avatar border border-dark" alt=""> SOSO <span class="fs-6 fw-normal">1 semaine</span> </h5>
 
-                <img src="../../assets/img/users/11/chien.png" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <div class="card-body text-start">
-                        <a href="#" class="card-link"><i class="bi bi-suit-heart-fill"></i></a>
-                        <a href="#" class="card-link"><i class="bi bi-chat-fill"></i></a>
-                        <p class="card-text">500 J'aime</p>
+            <?php foreach ($allPosts as $value) { ?>
 
-                        <p class="card-text w-100 text-truncate"><img src="../../assets/img/users/11/voiture.png" class="rounded-circle avatar border border-dark me-1" alt=""><b>SOSO</b> Jolie petit chien trop mignon jg jg jg jg jg jg j gjg j</p>
-                        <p class="card-text m-0 text-secondary text-truncate">Afficher les 20 commentaires</p>
-                        <p class="card-text m-0 text-secondary">Ajouter un commentaire...</p>
+                <div class="card col-lg-3 col-11">
+                    <h5 class="card-title"><img src="../../assets/img/users/11/voiture.png" class="rounded-circle avatar border border-dark" alt=""> <?= $value["user_pseudo"] ?> <span class="fs-6 fw-normal"><?= date("d/m/Y - H:i", $value['post_timestamp']) ?></span> </h5>
+
+                    <img src="../../assets/img/users/<?= $value["user_id"] ?>/<?= $value["pic_name"] ?>" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <div class="card-body text-start">
+                            <a href="#" class="card-link"><i class="bi bi-suit-heart"></i></a>
+                            <a href="#" class="card-link"><i class="bi bi-chat"></i></a>
+                            <p class="card-text">500 J'aime</p>
+
+                            <p class="card-text w-100 text-truncate"><img src="../../assets/img/users/11/voiture.png" class="rounded-circle avatar border border-dark me-1" alt=""><b> <?= $value["user_pseudo"] ?></b> <?= $value["post_description"] ?></p>
+                            <p class="card-text m-0 text-secondary text-truncate">Afficher les 20 commentaires</p>
+                            <p class="card-text m-0 text-secondary">Ajouter un commentaire...</p>
+
+                        </div>
 
                     </div>
-
                 </div>
 
-
-
-
-            </div>
-
-
-
-
-
-
-
-
-
-
+            <?php } ?>
 
         </div>
 
